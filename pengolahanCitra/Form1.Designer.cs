58083668c31728826bf8a10a98a5dbd6ae313ed0
agunggumel <agung.gumelarf2011@gmail.com>
2021-11-03 04:18:19 -0700

    p

---
+++ pengolahanCitra/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace pengolahanCitra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(241, 281);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(259, 39);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(250, 281);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(12, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(515, 96);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 28);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Biner";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(515, 67);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 28);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "GrayScale";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.Black;
+            this.button5.Location = new System.Drawing.Point(515, 125);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 28);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Brightness";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(490, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Transparent;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ForeColor = System.Drawing.Color.Black;
+            this.button6.Location = new System.Drawing.Point(515, 184);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 28);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Invers";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Transparent;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.ForeColor = System.Drawing.Color.Black;
+            this.button7.Location = new System.Drawing.Point(515, 213);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 28);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Contrast";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Transparent;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.ForeColor = System.Drawing.Color.Black;
+            this.button8.Location = new System.Drawing.Point(515, 242);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 28);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "Negative";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Transparent;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.ForeColor = System.Drawing.Color.Black;
+            this.button9.Location = new System.Drawing.Point(515, 154);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 28);
+            this.button9.TabIndex = 14;
+            this.button9.Text = "M-bit";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(437, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Angka";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(198, 23);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Citra Awal";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(255, 23);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Hasil citra";
+            // 
+            // Form1
+            // 
+            this.AccessibleDescription = "";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(607, 334);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Pengolahan Citra";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ pengolahanCitra/Form1.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace pengolahanCitra
+{
+    public partial class Form1 : Form
+    {
+        Bitmap gam, gambar2;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofile = new OpenFileDialog();
+            if (ofile.ShowDialog() == DialogResult.OK)
+            {
+                gam = new Bitmap(new Bitmap(ofile.FileName), pictureBox1.Width, pictureBox1.Height);
+                pictureBox1.Image = gam;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i, j, rata, R, G , B;
+            if (gam != null)
+            {
+                gambar2 = new Bitmap(gam);
+                for (i = 0; i <= gambar2.Width - 1; i++)
+                {
+                    for (j = 0; j <= gambar2.Height - 1; j++)
+                    {
+                        Color pixelColor = gambar2.GetPixel(i, j);
+                        R = pixelColor.R;
+                        G = pixelColor.G;
+                        B = pixelColor.B;
+                        rata = Convert.ToInt32((R + G + B) / 3);
+                        if (rata > 127)
+                            rata = 255;
+                        else
+                            rata = 0;
+                        Color newpixelColor = Color.FromArgb(rata, rata, rata);
+                        gambar2.SetPixel(i, j, newpixelColor);
+                    }
+                }
+                pictureBox2.Image = gambar2;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int i, j, rata, R, G,Blu;
+            if (gam != null)
+            {
+                gambar2 = new Bitmap(gam);
+                for (i = 0; i <= gambar2.Width - 1; i++)
+                {
+                    for (j = 0; j <= gambar2.Height - 1; j++)
+                    {
+                        Color pixelColor = gambar2.GetPixel(i, j);
+                        R = pixelColor.R;
+                        G = pixelColor.G;
+                        Blu = pixelColor.B;
+                        rata = Convert.ToInt32((merah + hijau + Blu) / 3);
+
+                        Color newpixelColor = Color.FromArgb(rata, rata, rata);
+                        gambar2.SetPixel(i, j, newpixelColor);
+                    }
+                }
+                pictureBox2.Image = gambar2;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text==""){
+                MessageBox.Show("Insert Angka");
+            
+            
+                }else{
+                    Bitmap bmp = new Bitmap(pictureBox1.Image);
+                    Bitmap bmp2 = new Bitmap(pictureBox1.Image);
+                    int r, g, b;
+                    Color c;
+                    int val = (int)Convert.ToInt32(textBox1.Text);
+                    Cursor = Cursors.WaitCursor;
+                    for (int i = 0; i < bmp.Width; i++)
+                    {
+                        for (int j = 0; j < bmp.Height; j++)
+                        {
+                            c = bmp.GetPixel(i, j);
+                            r = c.R + val;
+                            g = c.G + val;
+                            b = c.B + val;
+                            if (r > 255) r = 255;
+                            if (g > 255) g = 255;
+                            if (b > 255) b = 255;
+                            if (r < 0) r = 0;
+                            if (g < 0) g = 0;
+                            if (b < 0) b = 0;
+                            bmp2.SetPixel(i, j, Color.FromArgb(r, g, b));
+                        }
+                    }
+                    pictureBox2.Image = bmp2;
+                    Cursor = Cursors.Default;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int i, j, rata, R, G, B;
+          
+            if (gam != null)
+            {
+                gambar2 = new Bitmap(gam);
+                for (i = 0; i <= gambar2.Width - 1; i++)
+                {
+                    for (j = 0; j <= gambar2.Height - 1; j++)
+                    {
+                        Color pixelColor = gambar2.GetPixel(i, j);
+                       
+                        R = pixelColor.R;
+                        G = pixelColor.G;
+                        B = pixelColor.B;
+                        rata = Convert.ToInt32((R + G + B) / 3);
+                        int xi = 255 - rata;
+
+
+                        Color newpixelColor = Color.FromArgb(xi, xi, xi);
+                        gambar2.SetPixel(i, j, newpixelColor);
+                    }
+                }
+                pictureBox2.Image = gambar2;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int i, j;
+            int R, G, B;
+            int cR, cG, cB;
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Insert Angka");
+
+
+            }
+            else
+            {
+            float c = Convert.ToSingle(textBox1.Text); 
+            float[] h = new float[256];
+            
+            
+            if (gam!= null)
+            {
+                gambar2 = new Bitmap(gam);
+                for (i = 0; i <= gambar2.Width - 1; i++)
+                {
+                    for (j = 0; j <= gambar2.Height - 1; j++)
+                    {
+                        Color pixelColor = gambar2.GetPixel(i, j);
+                        R = pixelColor.R;
+                        G = pixelColor.G;
+                        B = pixelColor.B;
+
+                        int xg = (int)((pixelColor.R + pixelColor.G + pixelColor.B) / 3);
+                        cRed = Convert.ToInt32(R*c);
+                        cGreen = Convert.ToInt32(G * c);
+                        cBlue = Convert.ToInt32(B * c);
+                        int xc = (int)(c*xg);
+
+                        if (cR >= 255 || cG>=255 || cB>=255)
+                        {
+                            cR = 255;
+                            cG=255;
+                            cB=255;
+                            Color newpixelColor = Color.FromArgb(cR, cG, cB);
+                            gambar2.SetPixel(i, j, newpixelColor);
+                         
+                        }
+                        
+                       
+                    }
+                }
+                pictureBox2.Image = gambar2;
+            }
+                }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int i, j;
+            int R, G, B;
+            int nr, ng, nb;
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Insert Number");
+            }
+            else { 
+            int val = (int)Convert.ToInt32(textBox1.Text);
+            if (gam != null)
+            {
+                gambar2 = new Bitmap(gam);
+                for (i = 0; i <= gambar2.Width - 1; i++)
+                {
+                    for (j = 0; j <= gambar2.Height - 1; j++)
+                    {
+                      Color pixelColor = gambar2.GetPixel(i, j);
+                         
+                       R = pixelColor.R;
+                       G = pixelColor.G;
+                       B = pixelColor.B;
+                       nr = val - R;
+                       ng = val - G;
+                       nb = val - B;
+                            if (nr < 0) { 
+                                nr = 0;
+                            }
+                            else
+                            {
+                                nr = nr;
+                            }
+                            if (ng < 0) { 
+                                ng = 0;
+                            }
+                            else
+                            {
+                                ng = ng;
+                            }
+                            if (nb < 0) { 
+
+                                nb = 0;
+                            }
+                            else
+                            {
+                                nb = nb;
+                            }
+                            
+                            Color newpixelColor = Color.FromArgb(nr, ng, nb);
+                            gambar2.SetPixel(i, j, newpixelColor);
+
+                    
+                        }
+                    
+                                            
+                }
+                pictureBox2.Image = gambar2;
+            }
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+           int i, j, R, G, B;
+            int mR, mG, mB;
+           if (textBox1.Text == "")
+           {
+               MessageBox.Show("Insert Number");
+
+
+           }
+           else if (Convert.ToInt32(textBox1.Text) >= 9)
+           {
+               MessageBox.Show("Range untuk m-bit pangkat maks 8 (2^8 =256 RGB)");
+           }
+           else
+           { 
+            if (gam != null)
+            {
+                gambar2 = new Bitmap(gam);
+
+                for (i = 0; i <= gambar2.Width - 1; i++)
+                {
+                    for (j = 0; j <= gambar2.Height - 1; j++)
+                    {
+                        int val = Convert.ToInt32(textBox1.Text);
+                        double bit = Math.Pow(2, val);
+                        Color pixelColor = gambar2.GetPixel(i, j);
+                        R = pixelColor.R;
+                        G = pixelColor.G;
+                        B = pixelColor.B;
+                        
+                        
+                       
+                      
+                        mR = Convert.ToInt32(bit*(R/bit));
+                        mG = Convert.ToInt32(bit*(G/bit));
+                        mB = Convert.ToInt32(bit*(B/bit));
+                        
+                        int b1 = (int)(bit - 1);
+
+                        if (mR >= b1 && mR!=0)
+                            mR = 255;
+                        else
+                            mR = 0;
+
+                        if (mG >= b1 && mG!=0)
+                            mG = 255;
+                        else
+                            mG = 0;
+
+                        if (mB >= b1 && mG!=0)
+                            mB = 255;
+                        else
+                            mB = 0;
+                       
+                        Color newpixelColor = Color.FromArgb(mR,mG,mB);
+                        gambar2.SetPixel(i, j, newpixelColor);
+                    }
+                }
+                pictureBox2.Image = gambar2;
+        }
+           }
+    }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+}
+    }
